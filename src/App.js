8f8447fc288cb1ba0f9acb950b911d34893a6e6e
unknown <scott.wilson@evolo.co.uk>
2020-02-17 15:02:56 +0000

    feature chnage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>a.
+          Edit <code>src/App.js</code>change baby
         </p>
         <a
           className="App-link"
